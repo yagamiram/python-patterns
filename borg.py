246eac2280d0d56de999560f035c8dd0beaa07a5
Yagamiram Elric TK <sriram.ssnit@gmail.com>
2016-02-28 16:00:24 -0600

    Update borg.py

--- borg.py
+++ borg.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+'''
+This is an example of singletone class. Only one instance for the class.
+But the singleton is achieved in different way
 
+You are allowed to created as many instance for a class but the instances wont be unique. 
+The instances are shared to each other so if you make any changes it will affect all other instances.
+
+How this is achieved?
+By declaring a variable outside the class and refering it to the memeber variable declared inside the init function.
+
+Here, the __shared_state dict is declared outside the init fn and it is pointed by the variabel __dict__ which is declared
+inside the init function. 
+
+How come this helps to achieve singleton ?
+https://stackoverflow.com/questions/1537202/variables-inside-and-outside-of-a-class-init-function/1537226#1537226
+
+Variable set outside __init__ belong to the class. They're shared by all instances.
+
+Variables created inside __init__ (and all other method functions) and prefaced with self. belong to the object instance.
+
+And pointing the shared variable by a variable declared inside the init fn made all instance of the class as one single instance.
+'''
 
 class Borg:
     __shared_state = {}
